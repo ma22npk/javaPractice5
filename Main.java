dc861d6d03144804ac17e643a4c015872024cde6
Kota Matsumoto <k.matsumoto@blue-corporation.jp>
2020-10-05 11:46:04 +0900

    printDataメソッドを呼び出しガソリン残量を表呼び出しガソリン残量を表示

--- Main.java
+++ Main.java
@@ -12,16 +12,13 @@ class Main {
 
     System.out.println("【車の情報】");
     car.printData();
-    // getFuelメソッドを用いて「ガソリン量：◯◯L」と出力してください
-    System.out.println("ガソリン量：" + car.getFuel() + "L");
-
+    // 「ガソリン量：○○L」と出力している箇所を消してください
     System.out.println("-----------------");
     System.out.print("給油する量を入力してください：");
     int litre = scanner.nextInt();
-    // chargeメソッドを呼び出して、引数にlitreを渡してください
     car.charge(litre);
 
-    System.out.println("=================");
+    System.out.println("-----------------");
     System.out.println("【自転車の情報】");
     bicycle.printData();
   }
