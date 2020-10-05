0bdbaf9aae06357c105a17d60cfc1d4c6f5cdacb
Kota Matsumoto <k.matsumoto@blue-corporation.jp>
2020-10-05 09:56:44 +0900

    サブクラスをカスタマイズ

--- Main.java
+++ Main.java
@@ -1,22 +1,27 @@
+import java.util.Scanner;
+
 class Main {
   public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
     Car car = new Car();
-    // carに対してsetNameメソッドを呼び出し、引数に「フェラーリ」を渡してください
     car.setName("フェラーリ");
-    // carに対してsetColorメソッドを呼び出し、引数に「赤」を渡してください
     car.setColor("赤");
-
     Bicycle bicycle = new Bicycle();
-    // bicycleに対してsetNameメソッドを呼び出し、引数に「ビアンキ」を渡してください
     bicycle.setName("ビアンキ");
-
-    // bicycleに対してsetColorメソッドを呼び出し、引数に「緑」を渡してください
     bicycle.setColor("緑");
 
     System.out.println("【車の情報】");
     car.printData();
+    // getFuelメソッドを用いて「ガソリン量：◯◯L」と出力してください
+    System.out.println("ガソリン量：" + car.getFuel() + "L");
 
     System.out.println("-----------------");
+    System.out.print("給油する量を入力してください：");
+    int litre = scanner.nextInt();
+    // chargeメソッドを呼び出して、引数にlitreを渡してください
+    car.charge(litre);
+
+    System.out.println("=================");
     System.out.println("【自転車の情報】");
     bicycle.printData();
   }
